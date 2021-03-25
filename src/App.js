f7b4569245c251f0db1b6753332cbb80547e73db
Prashant-SK <psk.dev.ca@gmail.com>
2021-03-24 20:47:32 -0700

    created greeter component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
+import Greeter from './Greeter'
 
 function App() {
   return (
     <div>
     <h1>Hello World</h1>
     <Navbar/>
+    <Greeter name="Prashant"/>
     </div>
   );
 }
