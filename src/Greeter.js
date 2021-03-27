5759463887976a04d75955ea9d2def8b50e407c6
Prashant-SK <psk.dev.ca@gmail.com>
2021-03-27 14:39:13 -0700

    experimenting with adding buttons and onClick

--- src/Greeter.js
+++ src/Greeter.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 function Greeter(props){
+
     return (
     <>
     <h1>Welcome back, {props.name}. Age = {props.age}, sID = {props.age}</h1>
+    <button onClick={() => alert("Hello!")}>Click Me</button>
     </>
     )
 }
