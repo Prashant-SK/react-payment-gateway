e054c70e07ea722a6157cf2d798a50c3a83d30cc
Prashant-SK <psk.dev.ca@gmail.com>
2021-03-31 21:37:27 -0700

    change state with props to increment counter

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Counter from './Counter'
 function App() {
   return (
     <div>
-    <Counter/>
+    <Counter step={5}/>
     <h1>Hello World</h1>
     <Navbar/>
     <Greeter name="Prashant" age="24" sID={1}/>
