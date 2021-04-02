f0bc0a97ef8689b38522fcc347e6a7f2cb50a774
Prashant-SK <psk.dev.ca@gmail.com>
2021-04-01 21:19:57 -0700

    refactored code and added new component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
-import Greeter from './Greeter'
-import Counter from './Counter'
+import Greeter from './Greeter';
+import Counter from './Counter';
+import MoodToggler from './MoodToggler';
 
 function App() {
   return (
     <div>
-    <Counter step={5}/>
+    <MoodToggler step={5}/>
+    <Counter/>
     <h1>Hello World</h1>
     <Navbar/>
     <Greeter name="Prashant" age="24" sID={1}/>
--- src/Counter.js
+++ src/Counter.js
@@ -2,13 +2,10 @@ import React, {useState} from "react";
 
 function Counter({step = 1}){
 const [count, setCount] = useState(0);
-const [isHappy, setIsHappy] = useState(true);
-const toggleIsHappy = () => setIsHappy(!isHappy);
 return (
     <div>
         <h2>{count}</h2>
         <button onClick={() => setCount(count + step)}>+{step}</button>
-        <h3 onClick={toggleIsHappy}>{isHappy ? ":)" : ":("}</h3>
     </div>
 )
 }
