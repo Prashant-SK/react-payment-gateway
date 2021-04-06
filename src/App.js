a7ea08484e7ae81b6ae6c3e784a5e97591ff7dab
Prashant-SK <psk.dev.ca@gmail.com>
2021-04-06 11:59:29 -0700

    created a Cart Component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import Navbar from './Navbar';
 import Greeter from './Greeter';
 import Counter from './Counter';
 import MoodToggler from './MoodToggler';
+import Cart from './Cart';
 import './App.css'
+
+const items = [
+  { id: 1, name: 'Taco Shells', price: 3.25, qty: 2 },
+  { id: 2, name: 'Pinto Beans', price: 2.00, qty: 2 },
+  { id: 3, name: 'TSour Cream', price: 3.50, qty: 2 },
+]
+
 
 function App() {
   return (
     <div>
+    <Cart items={items}/>
     <MoodToggler step={5}/>
     <Counter/>
     <h1>Hello World</h1>
