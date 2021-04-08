468d133d2002c0833fcad94fa49bf9ecef3d91fc
Prashant-SK <psk.dev.ca@gmail.com>
2021-04-08 14:10:21 -0700

    Added buttons to enable users to select quantity

--- src/Cart.js
+++ src/Cart.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CartItem from './CartItem';
 
 function Cart({ initialItems }){
 
-    const grandTotal = initialItems.reduce((total, item) => (total + item.qty * item.price), 0).toFixed(2);
+    const [items, setItems] = useState(initialItems);
+
+    const updateQty = (id, newQty) => {
+        const newItems = items.map(item => {
+            if(item.id === id){
+                return {...item, qty: newQty}
+            }
+            return item;
+        });
+        setItems(newItems);
+    }
+
+    const grandTotal = items.reduce((total, item) => (total + item.qty * item.price), 0).toFixed(2);
     return (
         <div className="Cart">
             <h1>I am Cart</h1> 
             <div className="Cart-items">
-                {initialItems && initialItems.map(item => (
+                {items.map(item => (
                     <CartItem key={item.id} {...item}/>
                 ))}
             </div>
